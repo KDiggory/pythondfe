773544441d9eaf86f8144c4241eb88df62a0f055
Katie Diggory <katie.diggory@gmail.com>
2021-09-08 12:28:48 +0100

    update of importing classes

--- classes/classesexercise2.py
+++ classes/classesexercise2.py
@@ -26,11 +26,11 @@ vowelNum = wordcheck.check("Check this")
 descenders = testthis.check("Does this work. Has it got any descenders?")
 letterA = letterA.check("Check for any of the first letter of the alphabet")
 
-print(f"There are {vowelNum} vowels in your word")
-print(f"There are {descenders} of the letters in your word") # should only return one 
-print(f"There are {letterA} letter A's in your word")
+#print(f"There are {vowelNum} vowels in your word")
+#print(f"There are {descenders} of the letters in your word") # should only return one 
+#print(f"There are {letterA} letter A's in your word")
 
-
+## When this is used as a module to import comment out the print statements else you get those too!
 
 
                 
--- classes/importingclass.py
+++ classes/importingclass.py
@@ -5,3 +5,4 @@ letters = classesexercise2.VowelCheck()
 letters.toCheck = "K"
 
 kNum = letters.check("Checking this for the letter K")
+print(f"There are {kNum} letter K's in your string")
